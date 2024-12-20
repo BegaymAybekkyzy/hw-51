4124c73757e626615c054881ee593c1a3a5a2350
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2024-12-20 13:44:18 +0600

    Creating the basic logic of random number generation

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,27 @@ import { useState } from 'react';
 import Number from './components/Number/Number';
 
 const App = () => {
-  const [numbers] = useState<number[]>([5, 11, 16, 23, 32]);
+  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+  const randomNumberGenerator = (min: number, max: number) => {
+    const copyNumbers = [...numbers];
+    for (let i = 0; i < numbers.length; i++) {
+      let copyNumber: number = numbers[i];
+      copyNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      if (!copyNumbers.includes(copyNumber)) {
+        copyNumbers[i] = copyNumber;
+      }
+    }
+
+    copyNumbers.sort((a, b) => a - b);
+    setNumbers(copyNumbers);
+  };
+
   return (
     <>
       <div className="btnBlock">
-        <button>New numbers</button>
+        <button onClick={() => randomNumberGenerator(5, 36)}>New numbers</button>
       </div>
 
       {numbers.map((number) => (
